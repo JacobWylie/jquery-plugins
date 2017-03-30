41c7ad23cef79227477f567bfee4c299e582828f
JacobWylie <jacobwylie@gmail.com>
2017-03-30 17:56:43 +0200

    added work sitcky with link

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# jquery-plugins
+<h1>Playing with jQuery plugins</h1>
+<h3>Plugins Used</h3>
+<ul>
+	<li><a href="http://git.blivesta.com/animsition/">Animsition</a></li>
+	<li><a href="http://stickyjs.com/"></a>Sticky</li>
+</ul>
+<h3>Other Stuff</h3>
+<ul>
+	<li><a href="http://learn.jquery.com/events/">Events</a></li>
+	<li><a href="https://learn.jquery.com/events/introduction-to-custom-events/">Custom Events</a></li>
+</ul>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// page load animations
 $(".animsition").animsition({
   inClass: 'fade-in',
   outClass: 'fade-out',
@@ -6,6 +7,30 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
-$('.sticky').sticky({
+
+// Sitcky main header
+$('.header').sticky({
 	topSpacing:0
+});
+
+$('.header').on('sticky-start', () => {
+	$('.description').html('We build <strong>Great!</strong> apps');
+});
+
+$('.header').on('sticky-end', () => {
+	$('.description').html('We build apps');
+});
+
+
+// Sticky /work header
+$('.work-sticky').sticky({
+	topSpacing:60
+});
+
+$('.work-sticky').on('sticky-start', () => {
+	$('.work-sticky').append("<a href='#' class='sticky-email'> Email Us</a>");
+});
+
+$('.work-sticky').on('sticky-end', () => {
+	$('.sticky-email').remove();
 });
